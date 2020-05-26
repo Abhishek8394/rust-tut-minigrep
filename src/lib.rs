cca6c015dfad7a36a5d5753f92bccffd8eb0e354
Abhishek Patel <5524161+Abhishek8394@users.noreply.github.com>
2020-05-25 20:05:59 -0700

    ch 13 - 03 - iterator style search + config

--- src/lib.rs
+++ src/lib.rs
@@ -11,19 +11,32 @@ pub struct Config {
 
 impl Config{
     
-    pub fn new(args:&[String]) -> Result<Config, &'static str>{
-        if args.len() < 3{
-            return Err("not enough arguments!");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str>{
+        // if args.len() < 3{
+        //     return Err("not enough arguments!");
+        // }
+        args.next();
+        let query = match args.next(){
+            Some(q) => q,
+            None => return Err("Query string not provided"),
+        };
 
-        let mut case_sensitive = true;
-        for arg in args{
-            if arg == "-i" {
-                case_sensitive = false;
-            }
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let filename = match args.next(){
+            Some(f) => f,
+            None => return Err("Filename not provided"),
+        };
+
+        let ci_arg = match args.next(){
+            Some(a) => a,
+            None => String::from(""),
+        };
+
+        let mut case_sensitive = ci_arg == String::from("-i");
+        // if arg == "-i" {
+        //     case_sensitive = false;
+        // }
+        // let query = args[1].clone();
+        // let filename = args[2].clone();
         let case_sensitive = match env::var("CASE_INSENSITIVE"){
             Ok(n) => {eprintln!("Got {} for case_insensitive", n); false},
             Err(_) => {case_sensitive}
@@ -43,13 +56,18 @@ pub fn search_case_insensitive<'a>(query: &str, text: &'a str) -> Vec<&'a str>{
     results
 }
 pub fn search<'a>(query: &str, text: &'a str) -> Vec<&'a str>{
-    let mut result : Vec<&str> = Vec::new();
-    for line in text.lines(){
-        if line.contains(query){
-            result.push(line);
-        }
-    }
-    result
+    // no mutable `results` var! This would allow parallel processing in future
+    // since we do not have to manage access to a mutable var.
+    text.lines()
+        .filter(|line|{line.contains(query)})
+        .collect()
+    // let mut result : Vec<&str> = Vec::new();
+    // for line in text.lines(){
+    //     if line.contains(query){
+    //         result.push(line);
+    //     }
+    // }
+    // result
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
